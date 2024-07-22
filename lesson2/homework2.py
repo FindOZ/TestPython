6031392537d92a196186a5b6d956203875b9fc54
FindOZ <lawyer712@gmail.com>
2024-07-22 09:04:08 +0200

    Update

--- lesson2/homework2.py
+++ lesson2/homework2.py
@@ -1,4 +1,20 @@
-print('1234567')
+print("Great")
+name = input("Enter your name")
+print("Great", name, "name")
+print(type(name))
+name = 7 + 7
+print(type(name))
+print(name)
+
+
+
+
+
+
+
+
+
+
 
 
 
